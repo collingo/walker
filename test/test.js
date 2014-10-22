4abf91e47abd460ba72948cd294aa2ce3d164586
Nick Collings <nick@collingo.com>
2014-10-22 17:03:52 +0100

    initial commit

---
+++ test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var expect = chai.expect;
+chai.use(sinonChai);
+
+//// SUT
+var walker = require('../src/walker');
+
+describe('walker', function() {
+
+});
